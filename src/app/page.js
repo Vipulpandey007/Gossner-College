52f133959ccc82184c9640497967d3d183608f8b
Vipulpandey007 <vipulpandey708@gmail.com>
2024-03-18 22:28:43 +0530

    home page links added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="min-h-screen text-center mt-52">
+      Welcome to homepage
+      <Link href="/login" className="mx-10 text-blue-600">
+        Login
+      </Link>
+      <Link href="/profile" className="mx-10 text-blue-600">
+        Profile
+      </Link>
+    </div>
+  );
 }
