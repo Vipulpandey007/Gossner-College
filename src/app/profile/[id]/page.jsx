f6f040a95e68db39c4be75e66326068e3abe6cbc
Vipulpandey007 <vipulpandey708@gmail.com>
2024-03-18 22:18:15 +0530

    profile page added with dedicated user

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = ({ params }) => {
-  return <div>{params.id}</div>;
+  return (
+    <div className="bg-black text-white text-center min-h-screen">
+      Profile of this user:-{params.id}
+    </div>
+  );
 };
 
 export default page;
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>Profile</div>;
+  return <div>Profile Page</div>;
 };
 
 export default page;
