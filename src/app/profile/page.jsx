ed984de7da10588fc50d7e5a4d44cb0020f77cd2
Vipulpandey007 <vipulpandey708@gmail.com>
2024-03-24 22:12:11 +0530

    logout done successfully

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,36 @@
+"use client";
 import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const page = () => {
-  return <div>Profile Page</div>;
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  return (
+    <>
+      <div>
+        Profile Page
+        <hr />
+        <button
+          onClick={logout}
+          className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded mt-4"
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default page;
