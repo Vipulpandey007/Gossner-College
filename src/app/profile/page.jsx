99348d2bb68dbc4314cff6a31e44ab640b36c5aa
Vipulpandey007 <vipulpandey708@gmail.com>
2024-03-26 12:35:47 +0530

    profile extracted from database

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,12 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 const page = () => {
+  const [userData, setUserData] = useState("nothing");
   const router = useRouter();
   const logout = async () => {
     try {
@@ -17,11 +18,26 @@ const page = () => {
       toast.error(error.message);
     }
   };
+  useEffect(() => {
+    const getUsers = async () => {
+      const user = await axios.get("/api/users/user");
+      console.log(user);
+      setUserData(user.data.data.username);
+    };
+    getUsers();
+  }, []);
   return (
     <>
       <div>
         Profile Page
         <hr />
+        <h2>
+          {userData === "Nothing" ? (
+            "No user"
+          ) : (
+            <Link href={`/profile/${userData}`}>{userData}</Link>
+          )}
+        </h2>
         <button
           onClick={logout}
           className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded mt-4"
