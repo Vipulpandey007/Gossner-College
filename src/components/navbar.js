d1b21536b281c3749effb6bd5f3695fea4946dce
Vipulpandey007 <vipulpandey708@gmail.com>
2024-06-10 11:02:18 +0530

    some fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,7 +18,7 @@ import { useRouter } from "next/navigation";
 const Navbar = () => {
   const router = useRouter();
   const navigation = [
-    { name: "Dashboard", href: "#", current: true },
+    //{ name: "Dashboard", href: "#", current: true },
     // { name: "Team", href: "#", current: false },
     // { name: "Projects", href: "#", current: false },
     // { name: "Calendar", href: "#", current: false },
@@ -57,13 +57,6 @@ const Navbar = () => {
                 </DisclosureButton>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
@@ -85,15 +78,6 @@ const Navbar = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
