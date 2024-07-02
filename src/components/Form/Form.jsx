bb9ae066fa46cb1d0ec8b991907cae25af2dad4f
Vipulpandey007 <vipulpandey708@gmail.com>
2024-07-02 11:55:35 +0530

    input box added in profile page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 const Form = () => {
   return (
-    <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+    <div className="">
       <div className="flex flex-col gap-9">
         {/* <!-- Input Fields --> */}
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -18,29 +18,6 @@ const Form = () => {
                 type="text"
                 placeholder="Default Input"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              />
-            </div>
-
-            <div>
-              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Active Input
-              </label>
-              <input
-                type="text"
-                placeholder="Active Input"
-                className="w-full rounded-lg border-[1.5px] border-primary bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
-              />
-            </div>
-
-            <div>
-              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Disabled label
-              </label>
-              <input
-                type="text"
-                placeholder="Disabled label"
-                disabled
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary dark:disabled:bg-black"
               />
             </div>
           </div>
